bde4e87d012920642f88a6784b15a324167f0db2
tomash02 <70844011+tomash02@users.noreply.github.com>
2024-01-23 23:30:28 +0100

    Delete src directory

--- src/main/java/bdbt_bada_projekt/SpringApplication/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package bdbt_bada_projekt.SpringApplication.Repositories;
-
-
-import bdbt_bada_projekt.SpringApplication.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
--- src/main/java/bdbt_bada_projekt/SpringApplication/Services/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package bdbt_bada_projekt.SpringApplication.Services;
-
-public class UserAlreadyExistsException extends Exception {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
